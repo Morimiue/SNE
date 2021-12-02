fbc80b277341f151e3663c0288e2ed4087615cfc
MingZZX <mingzzx@outlook.com>
2021-12-02 15:21:11 +0800

    update model

--- model_gmlp.py
+++ model_gmlp.py
@@ -17,24 +17,26 @@ def get_y_hat(z):
 class GMLPModel(nn.Module):
     def __init__(self, in_dim, n_hidden_1, out_dim):
         super(GMLPModel, self).__init__()
-        self.layer1 = nn.Sequential(
-            nn.Linear(in_dim, n_hidden_1),
-            nn.PReLU(),
-            nn.LayerNorm(n_hidden_1, eps=1e-6),
-            nn.Dropout(0.35))
-        self.layer2 = nn.Sequential(
-            nn.Linear(n_hidden_1, out_dim))
+        self.linear1 = nn.Linear(in_dim, n_hidden_1)
+        self.act = nn.PReLU()
+        self.ln = nn.LayerNorm(n_hidden_1, eps=1e-6)
+        self.dp = nn.Dropout(0.35)
+        self.linear2 = nn.Linear(n_hidden_1, out_dim)
         self._init_weights()
 
     def _init_weights(self):
-        nn.init.xavier_uniform_(self.layer1.weight)
-        nn.init.xavier_uniform_(self.layer1.weight)
-        nn.init.normal_(self.layer1.bias, std=1e-6)
-        nn.init.normal_(self.layer1.bias, std=1e-6)
+        nn.init.xavier_uniform_(self.linear1.weight)
+        nn.init.xavier_uniform_(self.linear2.weight)
+        nn.init.normal_(self.linear1.bias, std=1e-6)
+        nn.init.normal_(self.linear2.bias, std=1e-6)
 
     def forward(self, x):
-        x = self.layer1(x)
-        x = self.layer2(x)
+        x = self.linear1(x)
+        x = self.act(x)
+        x = self.ln(x)
+        x = self.dp(x)
+        x = self.linear2(x)
+
         if self.training:
             return x, get_y_hat(x)
         else:
