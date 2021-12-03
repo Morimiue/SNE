ff7fe5ecdc7086c9bcbed48f8f3ebedcc9e57d21
MingZZX <mingzzx@outlook.com>
2021-12-03 14:51:38 +0800

    update model

--- model_gmlp.py
+++ model_gmlp.py
@@ -3,26 +3,16 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-def get_y_hat(z):
-    y_hat = z@z.T
-    mask = torch.eye(y_hat.shape[0])
-    z_sum = torch.sum(z**2, 1).reshape(-1, 1)
-    z_sum = torch.sqrt(z_sum).reshape(-1, 1)
-    z_sum = z_sum @ z_sum.T
-    y_hat = y_hat * (z_sum**(-1))
-    y_hat = (1-mask) * y_hat
-    return y_hat
-
-
 class GMLPModel(nn.Module):
     def __init__(self, in_dim, hidden_dim_1, out_dim):
         super(GMLPModel, self).__init__()
+
         self.linear1 = nn.Linear(in_dim, hidden_dim_1)
-        # self.act = nn.PReLU()
-        self.act = F.gelu
-        self.ln = nn.LayerNorm(hidden_dim_1, eps=1e-6)
-        self.dp = nn.Dropout(0.6)
+        self.act = nn.GELU()
+        self.ln = nn.LayerNorm(hidden_dim_1)
+        self.dp = nn.Dropout(0.35)
         self.linear2 = nn.Linear(hidden_dim_1, out_dim)
+
         self._init_weights()
 
     def _init_weights(self):
@@ -30,6 +20,15 @@ class GMLPModel(nn.Module):
         nn.init.xavier_uniform_(self.linear2.weight)
         nn.init.normal_(self.linear1.bias, std=1e-6)
         nn.init.normal_(self.linear2.bias, std=1e-6)
+
+    def _get_y_hat(self, z):
+        y_hat = z @ z.T
+        z_sum = torch.sum(z**2, 1).reshape(-1, 1)
+        z_sum = torch.sqrt(z_sum).reshape(-1, 1)
+        z_sum = z_sum @ z_sum.T
+        y_hat = y_hat * (z_sum**(-1))
+        y_hat = y_hat.fill_diagonal_(0.)
+        return y_hat
 
     def forward(self, x):
         x = self.linear1(x)
@@ -39,6 +38,6 @@ class GMLPModel(nn.Module):
         x = self.linear2(x)
 
         if self.training:
-            return x, get_y_hat(x)
+            return x, self._get_y_hat(x)
         else:
             return x
--- train_gmlp.py
+++ train_gmlp.py
@@ -3,11 +3,12 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import torch.optim as optim
+from torch.optim import Adam
 from torch.utils.data import DataLoader
 
 from model_gmlp import GMLPModel as Model
 from utils import *
+
 
 otu_path = './data/synthetic/otu0.csv'
 adj_path = './data/synthetic/adj0.csv'
@@ -15,7 +16,7 @@ wtd_adj_path = './data/synthetic/wtd_adj0.csv'
 model_path = './models/gmlp_model.pth'
 
 batch_size = 256
-epoch_num = 1
+epoch_num = 100
 learning_rate = 1e-2
 weight_decay = 5e-3
 delta = 0.10
@@ -36,42 +37,48 @@ class NContrastLoss(nn.Module):
         return loss
 
 
-# Train model
+# train model
 def train_model(model, data_loader):
-    # criterion = nn.MSELoss()
+    # define criterion and optimizer
     criterion = NContrastLoss()
-    optimizer = torch.optim.Adam(
-        model.parameters(), lr=learning_rate, weight_decay=weight_decay)
+    optimizer = Adam(model.parameters(), lr=learning_rate,
+                     weight_decay=weight_decay)
 
-    # Train on every epoch
+    # train for epochs
+    min_loss = float('inf')
     for epoch in range(epoch_num):
-        # Initialize loss and accuracy
+        # initialize loss and accuracy
         train_loss = 0.
         train_acc = 0.
 
+        # train on batches
         for _, data in enumerate(data_loader):
+            # get batch
             x, y = data
             if is_using_gpu:
-                x = x.cuda()
-                y = y.cuda()
-            # Forward
+                x, y = x.cuda(), y.cuda()
+            # forward
             z, y_hat = model(x)
-            # Backward
+            # backward
             optimizer.zero_grad()
             loss = criterion(y_hat, y) + F.mse_loss(y_hat, y)
             loss.backward()
             optimizer.step()
-            # Get loss and accuracy to print
+            # accumulate loss and accuracy
             train_loss += loss.item()
-            print(loss.item())
             train_acc += (abs(y-y_hat) < delta).float().mean()
 
-        # Print loss and accuracy
+        # get loss and accuracy of this epoch
         loader_step = len(data_loader)
+        train_loss = train_loss/loader_step
+        train_acc = train_acc/loader_step
+        min_loss = min(min_loss, train_loss)
+        # print tarining stats
         if epoch == 0 or (epoch+1) % 10 == 0:
             print(
-                f'--- Epoch: {epoch+1}, Loss: {train_loss/loader_step:.6f}, Acc: {train_acc/loader_step:.6f}')
+                f'--- Epoch: {epoch+1}, Loss: {train_loss:.6f}, Acc: {train_acc:.6f}')
 
+        # print some data for debug
         if (epoch+1) == epoch_num:
             np.set_printoptions(precision=3, suppress=True)
             if is_using_gpu:
@@ -83,29 +90,30 @@ def train_model(model, data_loader):
                 print(y_hat.detach().numpy().T)
                 print(y.numpy().T)
 
-    # Save model
+    # save last model
     if is_saving_model:
         torch.save(model.state_dict(), model_path)
 
 
-# otu, adj = read_raw_data(otu_path, adj_path)
-# spieces_num = otu.shape[0]
-# sample_num = otu.shape[1]
+if __name__ == '__main__':
+    # otu, adj = read_raw_data(otu_path, adj_path)
+    # spieces_num = otu.shape[0]
+    # sample_num = otu.shape[1]
 
-# train_dataset = get_emb_dateset()
+    # train_dataset = get_emb_dateset()
 
-# data_loader = DataLoader(dataset=train_dataset,
-#                          batch_size=batch_size, shuffle=False)
+    # data_loader = DataLoader(dataset=train_dataset,
+    #                          batch_size=batch_size, shuffle=False)
 
-# train_data = get_real_dataset()
-train_data = get_cora_dataset()
+    train_data = get_real_dataset()
+    # train_data = get_cora_dataset()
 
-data_loader = GMLPDataLoader(
-    data=train_data, batch_size=batch_size, shuffle=True)
+    data_loader = GMLPDataLoader(
+        data=train_data, batch_size=batch_size, shuffle=True)
 
-model = Model(1433, 256, 256)
+    model = Model(12, 256, 256)
 
-if is_using_gpu:
-    model = model.cuda()
+    if is_using_gpu:
+        model = model.cuda()
 
-train_model(model, data_loader)
+    train_model(model, data_loader)
