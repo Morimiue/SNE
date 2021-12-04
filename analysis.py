5e2174c29868f3f131a312c39664084b4fb20f38
MingZZX <mingzzx@outlook.com>
2021-12-04 21:02:23 +0800

    update get_p_values

--- analysis.py
+++ analysis.py
@@ -2,7 +2,7 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import scipy.stats as stats
+from scipy.stats import spearmanr
 from scipy.sparse import coo_matrix
 
 from model_gmlp import GMLPModel as Model
@@ -12,47 +12,43 @@ model_path = './models/gmlp_model.pth'
 
 is_using_gpu = torch.cuda.is_available()
 
-# calculate the permutation p-value of diffrernt methods
-# x: the series1 of the correlation calculation
-# y: the series2 of the correlation calculation
-# num_permutation: after how many permutations we get the p-value
-# method: which methods' p-value are we calculating?
-# y_hat: the predicted correlation value of our method between x and y, single value, not a matrix
 
+def get_p_values(x, perm_num, threshold, model):
+    '''
+    if the p-value is less than the threshold we reject the null-hypothesis,
+    and accept that our judgement is true i.e. these series are correlated
+    '''
+    # initial test statistics
+    pcc_init_score = np.corrcoef(x)
+    spm_init_score = spearmanr(x, axis=1)[0]
+    torch_x = torch.as_tensor(x, dtype=torch.float32)
+    if is_using_gpu:
+        torch_x.cuda()
+    sne_init_score = model(torch_x)[1].detach().numpy()
+    # permutated statistics
+    pcc_score = np.zeros((perm_num, x.shape[0], x.shape[0]))
+    spm_score = np.zeros((perm_num, x.shape[0], x.shape[0]))
+    sne_score = np.zeros((perm_num, x.shape[0], x.shape[0]))
+    for i in range(perm_num):
+        x = np.apply_along_axis(np.random.permutation, axis=1, arr=x)
+        pcc_score[i] = np.corrcoef(x)
+        spm_score[i] = spearmanr(x, axis=1)[0]
+        torch_x = torch.as_tensor(x, dtype=torch.float32)
+        if is_using_gpu:
+            torch_x.cuda()
+        sne_init_score = model(torch_x)[1].detach().numpy()
 
-def get_perm_pvalue(x, y, num_permutation, method, y_hat):
-    initial_test_statistic = np.abs(cal_corvalue(x, y, method, y_hat))
-    Scores_after_perm = np.zeros(num_permutation, dtype='float')
-    for i in range(0, num_permutation):
-        x = np.random.permutation(x)
-        y = np.random.permutation(y)
-        Scores_after_perm[i] = float(cal_corvalue(x, y, method, y_hat))
-    p_two_tail = np.sum(np.abs(Scores_after_perm) >= initial_test_statistic
-                        ) / np.float(num_permutation)
-    return p_two_tail
-
-
-# x: the series1 of the correlation calculation
-# y: the series2 of the correlation calculation
-# method: which methods' p-value are we calculating?
-
-
-def cal_corvalue(x, y, method, y_hat):
-    x = np.asarray(x)
-    y = np.asarray(y)
-    if method == 'Pearson':
-        return stats.pearsonr(x, y)[0]
-    elif (method == 'Spearman'):
-        return stats.spearmanr(x, y)[0]
-    elif (method == 'nbd'):
-        return y_hat
-    else:
-        print("Error: 相关度计算参数不正确，可选参数为：Pearson,Spearman,nbd")
-    return
-
-    # significance: if the p-value is less than the threshold
-    # we reject the null-hypothesis, and accept that our judgement is true
-    # which means these two series is correlated
+        if i % 100 == 0:
+            print(f'{i}th computation done')
+    # compute p values
+    pcc_p = np.sum(pcc_score > pcc_init_score, axis=0) / perm_num
+    spm_p = np.sum(spm_score > spm_init_score, axis=0) / perm_num
+    sne_p = np.sum(sne_score > sne_init_score, axis=0) / perm_num
+    #
+    pcc_ok = np.sum(pcc_p < threshold) / 2
+    spm_ok = np.sum(spm_p < threshold) / 2
+    sne_ok = np.sum(sne_p < threshold) / 2
+    print(pcc_ok, spm_ok, sne_ok)
 
 
 if __name__ == '__main__':
@@ -71,5 +67,4 @@ if __name__ == '__main__':
 
     model.eval()
 
-    torch_z, torch_y_hat = model(torch_data.x)
-    print(torch_y_hat)
+    get_p_values(x[:100], 1000, 0.01, model)
