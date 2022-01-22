0b8a0b10770fc6e11d6168df0782db643618daa2
archer_762 <1417835398@qq.com>
2022-01-22 09:57:16 +0800

    analysize the full graph in several batches

--- analyze.py
+++ analyze.py
@@ -1,3 +1,4 @@
+from math import perm
 import pickle
 
 import numpy as np
@@ -22,6 +23,8 @@ potential_loss_l = 2.
 
 is_use_gpu = torch.cuda.is_available()
 
+# is_use_gpu = False
+
 
 @torch.no_grad()
 def evaluate_model(x, y, model, perm_num, p_val_thresh, score_thresh):
@@ -42,8 +45,7 @@ def evaluate_model(x, y, model, perm_num, p_val_thresh, score_thresh):
     pcc_init_score = np.corrcoef(x)
     spm_init_score = spearmanr(x, axis=1)[0]
     lsa_init_score = lsa(x, 3)
-    mic_init_score = np.corrcoef(x)
-    # mic_init_score, _ = cstats(x, x, alpha=0.6, c=15, est="mic_e")
+    mic_init_score, _ = cstats(x, x, alpha=0.6, c=15, est="mic_e")
 
     # 设置对角线元素为0,以排除对角线元素干扰
     n = y.shape[0]
@@ -75,6 +77,11 @@ def evaluate_model(x, y, model, perm_num, p_val_thresh, score_thresh):
         lsa_score[i] = lsa(x, 3)
         mic_score[i] = np.corrcoef(x)
         # mic_score[i], _ = cstats(x, x, alpha=0.6, c=15, est="mic_e")
+        sne_score[i][range(x.shape[0]), range(x.shape[0])] = 0
+        pcc_score[i][range(x.shape[0]), range(x.shape[0])] = 0
+        spm_score[i][range(x.shape[0]), range(x.shape[0])] = 0
+        lsa_score[i][range(x.shape[0]), range(x.shape[0])] = 0
+        mic_score[i][range(x.shape[0]), range(x.shape[0])] = 0
 
         if i % 10 == 0:
             print(f'{i}th computation done')
@@ -148,98 +155,96 @@ def evaluate_model(x, y, model, perm_num, p_val_thresh, score_thresh):
 
 def evaluate_all_batches(p_val_thresh,
                          score_thresh,
-                         pcc_init_score,
-                         spm_init_score,
-                         sne_init_score,
+                         init_score_list,
                          p_sum_list,
                          perm_num=300,
                          in_path='p_value/scores.pkl'):
     # compute p values
-    print(p_sum_list.shape)
-    print(type(p_sum_list))
-    pcc_p = p_sum_list[0] / perm_num
-    spm_p = p_sum_list[1] / perm_num
-    sne_p = p_sum_list[2] / perm_num
-    print(sne_p.shape)
-    print(sne_init_score.shape)
+    p_list = p_sum_list / perm_num
     # the element with p value less than the threshold
-    # pcc_ok = np.sum(pcc_p < threshold) / 2
-    # spm_ok = np.sum(spm_p < threshold) / 2
-    # sne_ok = np.sum(sne_p < threshold) / 2
-    # 保留 p-value < 0.1 的元素
-    pcc_init_score *= (pcc_p < p_val_thresh)
-    spm_init_score *= (spm_p < p_val_thresh)
-    sne_init_score *= (sne_p < p_val_thresh)
+    init_score_list *= (p_list < p_val_thresh)
     # 判断是否相关，大于阈值则视为相关，得到相关矩阵
-    pcc_cor_matrix = (np.abs(pcc_init_score) > score_thresh).astype(float)
-    spm_cor_matrix = (np.abs(spm_init_score) > score_thresh).astype(float)
-    sne_cor_matrix = (np.abs(sne_init_score) > score_thresh).astype(float)
-
-    print("计算相关对数……")
-    pcc_intr_num = np.count_nonzero(pcc_cor_matrix)
-    spm_intr_num = np.count_nonzero(spm_cor_matrix)
-    sne_intr_num = np.count_nonzero(sne_cor_matrix)
+    cor_matrix_list = (np.abs(init_score_list) > score_thresh).astype(float)
     ground_truth_intr_num = np.count_nonzero(y)
-
+    intr_num_list = np.zeros((5, 1))
+    for i in range(5):
+        intr_num_list[i] = np.count_nonzero(cor_matrix_list[i])
     print('计算真阳性数……')
     # np.multiply(pcc_cor_matrix, y) 得到对位相乘结果，而 * 不行，离谱
-    pcc_true_pos = np.count_nonzero(np.multiply(pcc_cor_matrix, y))
-    spm_true_pos = np.count_nonzero(np.multiply(spm_cor_matrix, y))
-    sne_true_pos = np.count_nonzero(np.multiply(sne_cor_matrix, y))
+    true_pos_list = np.zeros((5, 1))
+    for i in range(5):
+        true_pos_list[i] = np.count_nonzero(np.multiply(cor_matrix_list[i], y))
 
     # 计算精确率，此时只需要考虑预测正确，且真的正确的值
     print('计算精确率……')
-    pcc_precision = pcc_true_pos / pcc_intr_num
-    spm_precision = spm_true_pos / spm_intr_num
-    sne_precision = sne_true_pos / sne_intr_num
-
+    precision_list = np.zeros((5, 1))
+    for i in range(5):
+        precision_list[i] = true_pos_list[i] / intr_num_list[i]
     # 计算召回率，此时只需要考虑真的正确，且我们预测正确的值
     print('计算召回率……')
-    pcc_recall = pcc_true_pos / ground_truth_intr_num
-    spm_recall = spm_true_pos / ground_truth_intr_num
-    sne_recall = sne_true_pos / ground_truth_intr_num
-
-    print(f'''{"":16}{"PCC":8}{"SPM":8}{"SNE":8}{"Ground Truth":12}
-{"Interactions":16}{pcc_intr_num:<8}{spm_intr_num:<8}{sne_intr_num:<8}{ground_truth_intr_num:<12}
-{"True Pos":16}{pcc_true_pos:<8}{spm_true_pos:<8}{sne_true_pos:<8}
-{"Precision":16}{pcc_precision:<8.2%}{spm_precision:<8.2%}{sne_precision:<8.2%}
-{"Recall":16}{pcc_recall:<8.2%}{spm_recall:<8.2%}{sne_recall:<8.2%}''')
+    recall_list = np.zeros((5, 1))
+    for i in range(5):
+        recall_list[i] = true_pos_list[i] / ground_truth_intr_num
+    intr_num_list = intr_num_list.astype(float)
+    print(
+        f'''{"":16}{"PCC":8}{"SPM":8}{"SNE":8}{"LSA":8}{"MIC":8}{"Ground Truth":12}
+{"Interactions":16}{intr_num_list[0][0]:<8}{intr_num_list[1][0]:<8}{intr_num_list[2][0]:<8}{intr_num_list[3][0]:<8}{intr_num_list[4][0]:<8}{ground_truth_intr_num:<12}
+{"True Pos":16}{true_pos_list[0][0]:<8}{true_pos_list[1][0]:<8}{true_pos_list[2][0]:<8}{true_pos_list[3][0]:<8}{true_pos_list[4][0]:<8}
+{"Precision":16}{precision_list[0][0]:<8.2%}{precision_list[1][0]:<8.2%}{precision_list[2][0]:<8.2%}{precision_list[3][0]:<8.2%}{precision_list[4][0]:<8.2%}
+{"Recall":16}{recall_list[0][0]:<8.2%}{recall_list[1][0]:<8.2%}{recall_list[2][0]:<8.2%}{recall_list[3][0]:<8.2%}{recall_list[4][0]:<8.2%}'''
+    )
 
 
 def calculate_p_in_batches(x,
                            perm_num,
                            model,
+                           init_score_list,
                            p_sum_list,
                            out_path='p_value/scores.pkl'):
     '''
     if the p-value is less than the threshold we reject the null-hypothesis,
     and accept that our judgement is true i.e. these series are correlated
-
-
     '''
-    pcc_score = np.zeros((perm_num, x.shape[0], x.shape[0]))
-    spm_score = np.zeros((perm_num, x.shape[0], x.shape[0]))
-    sne_score = np.zeros((perm_num, x.shape[0], x.shape[0]))
+    sne_score = np.empty((perm_num, x.shape[0], x.shape[0]))
+    pcc_score = np.empty((perm_num, x.shape[0], x.shape[0]))
+    spm_score = np.empty((perm_num, x.shape[0], x.shape[0]))
+    lsa_score = np.empty((perm_num, x.shape[0], x.shape[0]))
+    mic_score = np.empty((perm_num, x.shape[0], x.shape[0]))
+    zeros = torch.zeros(y.shape)
     for i in range(perm_num):
         x = np.apply_along_axis(np.random.permutation, axis=1, arr=x)
-        pcc_score[i] = np.corrcoef(x)
-        spm_score[i] = spearmanr(x, axis=1)[0]
         torch_x = torch.as_tensor(x, dtype=torch.float32)
         if is_use_gpu:
             torch_x = torch_x.cuda()
-            sne_score[i] = model(torch_x)[1].detach().cpu().numpy()
+            y_hat = model(torch_x)[1].cpu()
         else:
-            sne_score[i] = model(torch_x)[1].numpy()
-    # with open(out_path, 'a', newline='') as f:
-    #     pickle.dump(scores, f)
+            y_hat = model(torch_x)[1]
+        sne_score[i] = torch.maximum(
+            zeros, -(y_hat / potential_loss_l - 1)**2 + 1).detach().numpy()
+        pcc_score[i] = np.corrcoef(x)
+        spm_score[i] = spearmanr(x, axis=1)[0]
+        # lsa_score = lsa(x, 3)
+        lsa_score[i] = np.corrcoef(x)
+        # mic_score = cstats(x, x, alpha=0.6, c=15, est="mic_e")
+        mic_score[i] = np.corrcoef(x)
     # get the sum times when current_score is larger than init_score at this batch
-    pcc_p = np.sum(np.abs(pcc_score) > np.abs(pcc_init_score), axis=0)
-    spm_p = np.sum(np.abs(spm_score) > np.abs(spm_init_score), axis=0)
-    sne_p = np.sum(np.abs(sne_score) > np.abs(sne_init_score), axis=0)
+    pcc_p = np.sum(np.abs(pcc_score) > np.abs(init_score_list[0]), axis=0)
+    spm_p = np.sum(np.abs(spm_score) > np.abs(init_score_list[1]), axis=0)
+    sne_p = np.sum(np.abs(sne_score) > np.abs(init_score_list[2]), axis=0)
+    lsa_p = np.sum(np.abs(lsa_score) > np.abs(init_score_list[3]), axis=0)
+    mic_p = np.sum(np.abs(mic_score) > np.abs(init_score_list[4]), axis=0)
+    pcc_p[range(x.shape[0]), range(x.shape[0])] = 0
+    spm_p[range(x.shape[0]), range(x.shape[0])] = 0
+    sne_p[range(x.shape[0]), range(x.shape[0])] = 0
+    lsa_p[range(x.shape[0]), range(x.shape[0])] = 0
+    mic_p[range(x.shape[0]), range(x.shape[0])] = 0
+
     # get the sum times when current_score is larger than init_score at all batches
     p_sum_list[0] += pcc_p
     p_sum_list[1] += spm_p
     p_sum_list[2] += sne_p
+    p_sum_list[3] += lsa_p
+    p_sum_list[4] += mic_p
     return p_sum_list
 
 
@@ -247,19 +252,19 @@ def calculate_init_scores(x, y, model):
     # initial test statistics
     pcc_init_score = np.corrcoef(x)
     spm_init_score = spearmanr(x, axis=1)[0]
+    # lsa_init_score = lsa(x, 3)
+    lsa_init_score = np.corrcoef(x)
+    # mic_init_score, _ = cstats(x, x, alpha=0.6, c=15, est="mic_e")
+    mic_init_score = np.corrcoef(x)
     torch_x = torch.as_tensor(x, dtype=torch.float32)
     zeros = torch.zeros(y.shape)
     if is_use_gpu:
         torch_x = torch_x.cuda()
-        zeros = zeros.cuda()
         y_hat = model(torch_x)[1].cpu()
     else:
         y_hat = model(torch_x)[1]
-    y_hat = y_hat.detach().numpy()
-    sne_init_score = np.abs(y_hat)
-    # sne_init_score = F.relu(1 - y_hat / contrasive_loss_m).numpy()
-    # sne_init_score = torch.maximum(
-    #     zeros.cpu(), -(y_hat - potential_loss_l)**2 / 10000 + 1).numpy()
+    sne_init_score = torch.maximum(zeros, -(y_hat / potential_loss_l - 1)**2 +
+                                   1).detach().numpy()
     n = y.shape[0]
     sne_init_score = np.array(sne_init_score)
     # 设置对角线元素为0,以排除对角线元素干扰
@@ -267,7 +272,12 @@ def calculate_init_scores(x, y, model):
     pcc_init_score[range(n), range(n)] = 0
     spm_init_score[range(n), range(n)] = 0
     sne_init_score[range(n), range(n)] = 0
-    return pcc_init_score, spm_init_score, sne_init_score
+    lsa_init_score[range(n), range(n)] = 0
+    mic_init_score[range(n), range(n)] = 0
+    init_score_list = np.stack((pcc_init_score, spm_init_score, sne_init_score,
+                                lsa_init_score, mic_init_score),
+                               axis=0)
+    return init_score_list
 
 
 if __name__ == '__main__':
@@ -292,27 +302,29 @@ if __name__ == '__main__':
     model.load_state_dict(torch.load(model_path))
     model.eval()
 
-    # quick evaluate
-    x_tmp = x[:300].copy()
-    y_tmp = y[:300, :300].copy()
-    evaluate_model(x_tmp,
-                   y_tmp,
-                   model,
-                   perm_num=100,
-                   p_val_thresh=0.1,
-                   score_thresh=0.8)
+    # # quick evaluate
+    # x_tmp = x[:300].copy()
+    # y_tmp = y[:300, :300].copy()
+    # evaluate_model(x_tmp,
+    #                y_tmp,
+    #                model,
+    #                perm_num=100,
+    #                p_val_thresh=0.1,
+    #                score_thresh=0.8)
 
     # fully evaluate
-    # pcc_init_score, spm_init_score, sne_init_score = calculate_init_scores(
-    #     x, y, model)
-    # # 3 means methods that in our comparing experiment
-    # p_sum_list = np.zeros((3, x.shape[0], x.shape[0]))
-    # for i in range(10):
-    #     p_sum_list = calculate_p_in_batches(x, 10, model, p_sum_list)
-    #     print(f'{i*10}th computation done')
+    init_score_list = calculate_init_scores(x, y, model)
+    # 5 means methods that in our comparing experiment
+    p_sum_list = np.zeros((5, x.shape[0], x.shape[0]))
+    batch_of_perm = 10
+    batchsize_of_perm = 3
+    for i in range(batch_of_perm):
+        p_sum_list = calculate_p_in_batches(x, batchsize_of_perm, model,
+                                            init_score_list, p_sum_list)
+        print(f'{i*batch_of_perm}th computation done')
     # with open('p_value/scores.pkl', 'wb') as f:
     #     pickle.dump(p_sum_list, f)
     # with open('p_value/scores.pkl', 'rb') as f:
     #     p_sum_list = pickle.load(f)
-    # evaluate_all_batches(0.1, 0.8, pcc_init_score, spm_init_score,
-    #                      sne_init_score, p_sum_list)
+    evaluate_all_batches(0.1, 0.8, init_score_list, p_sum_list,
+                         batch_of_perm * batchsize_of_perm)
