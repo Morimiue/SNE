7a8d2fe99e2c8f420ca78b02ab23b83d1a4d5314
MingZZX <mingzzx@outlook.com>
2022-02-12 23:57:42 +0800

    reorganized analyze.py

--- analyze.py
+++ analyze.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import numpy as np
 import torch
 import torch.nn as nn
@@ -16,12 +18,15 @@ intr_path = './data/real/processed/interactions_trrust_tb.csv'
 # smpl_path = './data/synthetic/samples.csv'
 # intr_path = './data/synthetic/interactions.csv'
 
+results_dir_path = './data/inference/'
+
 feature_size = 40
 contrasive_loss_m = 10.
 potential_loss_l = 10.
 
-is_use_gpu = torch.cuda.is_available()
+# is_use_gpu = torch.cuda.is_available()
 is_use_gpu = False
+is_save_results = True
 
 
 def sne(x):
@@ -75,160 +80,108 @@ def lss(data: np.ndarray, D: int) -> np.ndarray:
 
 
 def lsa(x):
-    return np.corrcoef(x)
     return lss(x, 3)
 
 
 def mic(x):
-    return np.corrcoef(x)
     return cstats(x, x, alpha=0.6, c=15, est="mic_e")[0]
 
 
 @torch.no_grad()
-def predict(x, y, score_thresh):
+def predict_and_compare(x: np.ndarray,
+                        y: np.ndarray,
+                        methods: tuple[function, ...],
+                        score_thresh: float,
+                        p_val_thresh: float = 0.05,
+                        perm_num: int = 0) -> None:
     '''
     if the p-value is less than the threshold we reject the null-hypothesis,
     and accept that our judgement is true i.e. these series are correlated
     '''
-    # initial test statistics
-    sne_init_score = sne(x)
-
-    save_dense_to_interactions(sne_init_score,
-                               './data/predicted/interactions.csv')
-
-    # use upper triangular matrix to calculate
-    y = get_triu_items(y)
-    sne_init_score = get_triu_items(sne_init_score)
-
-    # 判断是否相关，大于阈值则视为相关，得到相关矩阵
-    sne_cor_matrix = np.abs(sne_init_score) > score_thresh
-
-    print("计算相关对数……")
-    sne_intr_num = np.count_nonzero(sne_cor_matrix)
-    ground_truth_intr_num = np.count_nonzero(y)
-
-    print('计算真阳性数……')
-    # np.multiply(pcc_cor_matrix, y) 得到对位相乘结果，而 * 不行，离谱
-    sne_true_pos = np.count_nonzero(np.multiply(sne_cor_matrix, y))
-
-    # 计算精确率，此时只需要考虑预测正确，且真的正确的值
-    print('计算精确率……')
-    sne_precision = sne_true_pos / sne_intr_num
-
-    # 计算召回率，此时只需要考虑真的正确，且我们预测正确的值
-    print('计算召回率……')
-    sne_recall = sne_true_pos / ground_truth_intr_num
-
-    print(f'''{"":16}{"SNE":8}{"Ground Truth":12}
-{"Interactions":16}{sne_intr_num:<8}{ground_truth_intr_num:<12}
-{"True Pos":16}{sne_true_pos:<8}
-{"Precision":16}{sne_precision:<8.2%}
-{"Recall":16}{sne_recall:<8.2%}''')
-
-
-@torch.no_grad()
-def predict_and_compare(x, y, perm_num, p_val_thresh, score_thresh):
-    '''
-    if the p-value is less than the threshold we reject the null-hypothesis,
-    and accept that our judgement is true i.e. these series are correlated
-    '''
-    # initial test statistics
-    sne_init_score = sne(x)
-    pcc_init_score = pcc(x)
-    spm_init_score = spm(x)
-    lsa_init_score = lsa(x)
-    mic_init_score = mic(x)
-
     np.fill_diagonal(y, 0.)
-    np.fill_diagonal(sne_init_score, np.nan)
-    np.fill_diagonal(pcc_init_score, np.nan)
-    np.fill_diagonal(spm_init_score, np.nan)
-    np.fill_diagonal(lsa_init_score, np.nan)
-    np.fill_diagonal(mic_init_score, np.nan)
+    # y = get_triu_items(y)
+    ground_truth_intr_count = np.count_nonzero(y)
+
+    init_scores = np.empty((len(methods), len(x), len(x)))
+    if perm_num > 0:
+        perm_scores = np.empty((len(methods), len(x), len(x)))
+        p_values = np.zeros((len(methods), len(x), len(x)))
+    cor_matrixs = np.empty((len(methods), len(x), len(x)))
+
+    intr_counts = np.empty(len(methods), dtype=np.int_)
+    true_posits = np.empty(len(methods), dtype=np.int_)
+    precisions = np.empty(len(methods))
+    recalls = np.empty(len(methods))
+
+    # initial test statistics
+    for i in range(len(methods)):
+        init_scores[i] = methods[i](x)
+        np.fill_diagonal(init_scores[i], np.nan)
 
     # permutated statistics
-    sne_p_count = 0
-    pcc_p_count = 0
-    spm_p_count = 0
-    lsa_p_count = 0
-    mic_p_count = 0
-    for i in range(perm_num):
-        x = np.apply_along_axis(np.random.permutation, axis=1, arr=x)
-        sne_perm_score = sne(x)
-        pcc_perm_score = pcc(x)
-        spm_perm_score = spm(x)
-        lsa_perm_score = lsa(x)
-        mic_perm_score = mic(x)
+    if perm_num > 0:
+        for i in range(perm_num):
+            x = np.apply_along_axis(np.random.permutation, axis=1, arr=x)
+            for j in range(len(methods)):
+                perm_scores[j] = methods[j](x)
+                p_values[j] += np.abs(perm_scores[j]) > np.abs(init_scores[j])
+            print(f'permutation test: {i+1} / {perm_num}', end='\r')
+        p_values /= perm_num
 
-        sne_p_count += np.abs(sne_perm_score) > np.abs(sne_init_score)
-        pcc_p_count += np.abs(pcc_perm_score) > np.abs(pcc_init_score)
-        spm_p_count += np.abs(spm_perm_score) > np.abs(spm_init_score)
-        lsa_p_count += np.abs(lsa_perm_score) > np.abs(lsa_init_score)
-        mic_p_count += np.abs(mic_perm_score) > np.abs(mic_init_score)
+    # compute the correlation matrix for each method
+    for i in range(len(methods)):
+        if perm_num > 0:
+            cor_matrixs[i] = np.multiply(
+                np.abs(init_scores[i]) >= score_thresh,
+                p_values[i] <= p_val_thresh)
+        else:
+            cor_matrixs[i] = np.abs(init_scores[i]) >= score_thresh
+        intr_counts[i] = np.count_nonzero(cor_matrixs[i])
+        true_posits[i] = np.count_nonzero(np.multiply(cor_matrixs[i], y))
+        precisions[i] = true_posits[i] / intr_counts[i]
+        recalls[i] = true_posits[i] / ground_truth_intr_count
 
-        if i % 10 == 0:
-            print(f'{i}th computation done')
+    # print results
+    print(f'{"":16}', end='')
+    for x in map(lambda x: x.__name__, methods):
+        print(f'{x:12}', end='')
+    print('Ground Truth')
 
-    # compute p values
-    sne_p = sne_p_count / perm_num
-    pcc_p = pcc_p_count / perm_num
-    spm_p = spm_p_count / perm_num
-    lsa_p = lsa_p_count / perm_num
-    mic_p = mic_p_count / perm_num
+    print(f'{"Interactions":16}', end='')
+    for x in intr_counts:
+        print(f'{x:<12}', end='')
+    print(ground_truth_intr_count)
 
-    # the elements with p value less than the threshold
-    sne_init_score *= (sne_p < p_val_thresh)
-    pcc_init_score *= (pcc_p < p_val_thresh)
-    spm_init_score *= (spm_p < p_val_thresh)
-    lsa_init_score *= (lsa_p < p_val_thresh)
-    mic_init_score *= (mic_p < p_val_thresh)
+    print(f'{"True Positives":16}', end='')
+    for x in true_posits:
+        print(f'{x:<12}', end='')
+    print()
 
-    # 判断是否相关，大于阈值则视为相关，得到相关矩阵
-    sne_cor_matrix = np.abs(sne_init_score) > score_thresh
-    pcc_cor_matrix = np.abs(pcc_init_score) > score_thresh
-    spm_cor_matrix = np.abs(spm_init_score) > score_thresh
-    lsa_cor_matrix = np.abs(lsa_init_score) > score_thresh
-    mic_cor_matrix = np.abs(mic_init_score) > score_thresh
+    print(f'{"Precision":16}', end='')
+    for x in precisions:
+        print(f'{x:<12.2%}', end='')
+    print()
 
-    print("计算相关对数……")
-    sne_intr_num = np.count_nonzero(sne_cor_matrix)
-    pcc_intr_num = np.count_nonzero(pcc_cor_matrix)
-    spm_intr_num = np.count_nonzero(spm_cor_matrix)
-    lsa_intr_num = np.count_nonzero(lsa_cor_matrix)
-    mic_intr_num = np.count_nonzero(mic_cor_matrix)
-    ground_truth_intr_num = np.count_nonzero(y)
+    print(f'{"Recall":16}', end='')
+    for x in recalls:
+        print(f'{x:<12.2%}', end='')
+    print()
 
-    print('计算真阳性数……')
-    # np.multiply(pcc_cor_matrix, y) 得到对位相乘结果，而 * 不行，离谱
-    sne_true_pos = np.count_nonzero(np.multiply(sne_cor_matrix, y))
-    pcc_true_pos = np.count_nonzero(np.multiply(pcc_cor_matrix, y))
-    spm_true_pos = np.count_nonzero(np.multiply(spm_cor_matrix, y))
-    lsa_true_pos = np.count_nonzero(np.multiply(lsa_cor_matrix, y))
-    mic_true_pos = np.count_nonzero(np.multiply(mic_cor_matrix, y))
+    # save results
+    if is_save_results:
+        np.save(results_dir_path + 'precisions.npy', precisions)
+        np.save(results_dir_path + 'recalls.npy', recalls)
 
-    # 计算精确率，此时只需要考虑预测正确，且真的正确的值
-    print('计算精确率……')
-    sne_precision = sne_true_pos / sne_intr_num
-    pcc_precision = pcc_true_pos / pcc_intr_num
-    spm_precision = spm_true_pos / spm_intr_num
-    lsa_precision = lsa_true_pos / lsa_intr_num
-    mic_precision = mic_true_pos / mic_intr_num
+        p_counts = np.empty(10, dtype=np.int_)
+        for i in range(10):
+            p_counts[i] = np.count_nonzero(p_values[0] < (i + 1) / 10)
+        for i in range(10 - 1, 0, -1):
+            p_counts[i] -= p_counts[i - 1]
+        np.save(results_dir_path + 'p_counts.npy', p_counts)
+        np.savetxt(results_dir_path + 'p_counts.txt', p_counts, fmt='%d')
 
-    # 计算召回率，此时只需要考虑真的正确，且我们预测正确的值
-    print('计算召回率……')
-    sne_recall = sne_true_pos / ground_truth_intr_num
-    pcc_recall = pcc_true_pos / ground_truth_intr_num
-    spm_recall = spm_true_pos / ground_truth_intr_num
-    lsa_recall = lsa_true_pos / ground_truth_intr_num
-    mic_recall = mic_true_pos / ground_truth_intr_num
-
-    print(
-        f'''{"":16}{"SNE":8}{"PCC":8}{"SPM":8}{"LSA":8}{"MIC":8}{"Ground Truth":12}
-{"Interactions":16}{sne_intr_num:<8}{pcc_intr_num:<8}{spm_intr_num:<8}{lsa_intr_num:<8}{mic_intr_num:<8}{ground_truth_intr_num:<12}
-{"True Pos":16}{sne_true_pos:<8}{pcc_true_pos:<8}{spm_true_pos:<8}{lsa_true_pos:<8}{mic_true_pos:<8}
-{"Precision":16}{sne_precision:<8.2%}{pcc_precision:<8.2%}{spm_precision:<8.2%}{lsa_precision:<8.2%}{mic_precision:<8.2%}
-{"Recall":16}{sne_recall:<8.2%}{pcc_recall:<8.2%}{spm_recall:<8.2%}{lsa_recall:<8.2%}{mic_recall:<8.2%}'''
-    )
+        save_dense_to_interactions(cor_matrixs[0],
+                                   results_dir_path + 'interactions.csv')
 
 
 if __name__ == '__main__':
@@ -246,15 +199,11 @@ if __name__ == '__main__':
     model.load_state_dict(torch.load(model_path))
     model.eval()
 
-    # ---------- quick evaluate ----------
-    predict(x, y, model, score_thresh=0.)
-
-    # ---------- fully evaluate ----------
-    # x_tmp = x[:300].copy()
-    # y_tmp = y[:300, :300].copy()
-    # predict_and_compare(x_tmp,
-    #                     y_tmp,
-    #                     model,
-    #                     perm_num=100,
-    #                     p_val_thresh=0.1,
-    #                     score_thresh=0.8)
+    x_tmp = x[:200].copy()
+    y_tmp = y[:200, :200].copy()
+    predict_and_compare(x_tmp,
+                        y_tmp,
+                        methods=(sne, pcc, spm),
+                        score_thresh=0.8,
+                        p_val_thresh=0.1,
+                        perm_num=100)
